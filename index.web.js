2dfc130b38f6e29a3959c70816ae40d338476b81
k.ikemizu <k.ikemizu@questry.co.jp>
2023-05-12 19:41:14 +0900

    init

--- index.web.js
+++
@@ -1,7 +0,0 @@
-import App from './App';
-import { name as appName } from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
-AppRegistry.runApplication(appName, {
-  rootTag: document.getElementById('root'),
-});
